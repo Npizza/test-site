28bd32bc97d5a03260fa082438c6bda445795a18
Gnik <it3@primer.gr>
2020-06-28 17:39:31 +0300

    fix I am

--- _js/vendors/jquery-3.5.1.slim.js
+++ _js/vendors/jquery-3.5.1.slim.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery JavaScript Library v3.5.1 -ajax,-ajax/jsonp,-ajax/load,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-manipulation/_evalUrl,-deprecated/ajax-event-alias,-effects,-effects/Tween,-effects/animatedSelector
  * https://jquery.com/
- * test
+ *
  * Includes Sizzle.js
  * https://sizzlejs.com/
  *
